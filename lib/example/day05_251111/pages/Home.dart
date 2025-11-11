9487c4b1c52f130883f4e3be70f4e8b513fca74b
OngTK <dhdxorud@gmail.com>
2025-11-11 13:22:31 +0900

    251111_1322 day05_spring연결_todo 수정

--- lib/example/day05_251111/pages/Home.dart
+++ lib/example/day05_251111/pages/Home.dart
@@ -49,40 +49,71 @@ class HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("TODO")),
-      body: Center( // 가운데 정렬
-        child: Column(  // 세로 정렬
-          children: [ Expanded( // 확장형 스크롤 제공
-            child: ListView(
-              // 반복문을 이용하여 ListView에 여러 개의 Card를 대입
-              // todoList.map( ( 반복변수명 ){ return 반환위젯( ) } ).toList()
-              children: todoList.map( (todo) {
-                // map 반복문은 dynamic 타입을 지원하지 않음
-                return Card(
-                  child: ListTile(
-                    title: Text(todo['title']),
-                    trailing:Row( // 가로정렬 / 가로 배치 위젯
-                      // row 배치에서 오른쪽 버튼들의 넓이를 자동으로 최소 크기로 할당
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        // 삭제 버튼
-                        IconButton(onPressed: (){delete(todo['id']);}, icon: Icon(Icons.delete)),
-                        // 수정 버튼
-                        IconButton(onPressed: (){ Navigator.pushNamed(context, "/update",arguments: todo['id']);}, icon: Icon(Icons.update)),
-                        // 상세 버튼
-                        // 위젯 간의 매개변수 전달 필요
-                        // Navigator.pushNamed(context, "/detail", arguments: 매개변수 );
-                        // context : 현재 위젯
-                        // "/detail" : 이동할 위젯 경로
-                        // arguments : 매개변수 전달
-                        IconButton(onPressed: (){ Navigator.pushNamed(context, "/detail", arguments: todo['id'] );}, icon: Icon(Icons.info))
-                      ],
+      body: Center(
+        // 가운데 정렬
+        child: Column(
+          // 세로 정렬
+          children: [
+            Expanded(
+              // 확장형 스크롤 제공
+              child: ListView(
+                // 반복문을 이용하여 ListView에 여러 개의 Card를 대입
+                // todoList.map( ( 반복변수명 ){ return 반환위젯( ) } ).toList()
+                children: todoList.map((todo) {
+                  // map 반복문은 dynamic 타입을 지원하지 않음
+                  return Card(
+                    child: ListTile(
+                      title: Text(todo['title']),
+                      trailing: Row(
+                        // 가로정렬 / 가로 배치 위젯
+                        // row 배치에서 오른쪽 버튼들의 넓이를 자동으로 최소 크기로 할당
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          // 삭제 버튼
+                          IconButton(
+                            onPressed: () {
+                              delete(todo['id']);
+                            },
+                            icon: Icon(Icons.delete),
+                          ),
+                          // 수정 버튼
+                          IconButton(
+                            onPressed: () {
+                              final result = Navigator.pushNamed(
+                                context,
+                                "/update",
+                                arguments: todo['id'],
+                              );
+                              // 업데이트 결과 pop이 true 라면 목록을 재조회
+                              if(result == true){findAll();}
+                            },
+                            icon: Icon(Icons.update),
+                          ),
+                          // 상세 버튼
+                          // 위젯 간의 매개변수 전달 필요
+                          // Navigator.pushNamed(context, "/detail", arguments: 매개변수 );
+                          // context : 현재 위젯
+                          // "/detail" : 이동할 위젯 경로
+                          // arguments : 매개변수 전달
+                          IconButton(
+                            onPressed: () {
+                              Navigator.pushNamed(
+                                context,
+                                "/detail",
+                                arguments: todo['id'],
+                              );
+                            },
+                            icon: Icon(Icons.info),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              } ).toList(), // map end
+                  );
+                }).toList(), // map end
+              ),
             ),
-          ),
-        ],)
+          ],
+        ),
       ),
     );
   } // build end
--- lib/example/day05_251111/pages/Update.dart
+++ lib/example/day05_251111/pages/Update.dart
@@ -11,12 +11,16 @@ class UpdateState extends State<Update> {
   // [1] 부모 위젯으로부터 매개변수 받기
   @override
   void didChangeDependencies() {
-    int id = ModalRoute.of(context)!.settings.arguments as int;
+    int id = ModalRoute
+        .of(context)!
+        .settings
+        .arguments as int;
     findById(id);
   } // didChangeDependencies func end
 
   // [2] to do 정보 가져오기
   dynamic todo = {};
+
   void findById(int id) async {
     try {
       final r = await dio.get("http://localhost:8080/api/todo/detail?id=${id}");
@@ -31,24 +35,66 @@ class UpdateState extends State<Update> {
       print("[findById] 실패 ${e}");
     }
   }
+
   // [3] 입력상자 제어 Controller
   TextEditingController titleCont = TextEditingController();
   TextEditingController contentCont = TextEditingController();
+
+  // [4] Spring 서버에게 업데이트 요청
+  void update() async {
+    try {
+      dynamic obj = {
+        "id": todo['id'],
+        "title": titleCont.text,
+        "content": contentCont.text,
+        "done" : todo['done']
+      };
+
+      final r = await dio.put("http://localhost:8080/api/todo", data: obj);
+      final d = await r.data;
+      if(d!=null){
+        // 수정성공
+        Navigator.pop(context, true);
+        // 성공시 현재 위젯을 pop = 뒤로 가기
+        // 반환값으로 true
+      }
+    } catch (e) {
+      print("[update 실패] ${e}");
+    }
+  }
 
   // build 구현부 =============================================================
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("To do 수정")),
-      body: Padding(padding: EdgeInsetsGeometry.all(30),
-          child: Column(
-            children: [
-              // maxLength 입력 길이 제한
-              TextField(controller: titleCont, maxLength: 30),
-              TextField(controller: contentCont, maxLength: 255)
-
-            ],
-          )
+      body: Padding(
+        padding: EdgeInsetsGeometry.all(30),
+        child: Column(
+          children: [
+            // maxLength 입력 길이 제한
+            TextField(controller: titleCont, maxLength: 30),
+            SizedBox(height: 20),
+            // 긴 글을 입력받을 때, 속성
+            TextField(
+              controller: contentCont,
+              maxLength: 255,
+              maxLines: 5, // 최대 세로 길이 ,5줄 초과시 자동으로 스크롤바 생성
+            ),
+            SizedBox(height: 20),
+            Text("완료 여부"),
+            Switch(
+              value: todo['done'],
+              onChanged: (value) {
+                setState(() {
+                  todo['done'] = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            OutlinedButton(onPressed: () {update();}, child: Text("수정하기")),
+          ],
+        ),
       ),
     );
   } // build end
